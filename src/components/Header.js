2d37433b6066ae05036078564d1834a6bbcc2136
HR Williams <halrubinwilliams@gmail.com>
2021-08-21 16:06:31 -0700

    Update image alt text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
   return (
     <React.Fragment>
       <h1>Ferment Collective Tap Room</h1>
-      <img src={champagneImage} alt="An image of jelly jar with champagne next to a candle" />
+      <img src={champagneImage} alt="An image of jelly jar with champagne on a wood table next to a tea light" />
       <p>In house application to track inventory of our house-made fermented drinks on tap. We sell unique house-made wines, champagnes and kombuchas, sourced from home grown and locally foraged ingredients</p>
     </React.Fragment>
   );
