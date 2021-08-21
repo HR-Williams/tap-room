850377f5a7d70384193776466c038ecd1866464f
HR Williams <halrubinwilliams@gmail.com>
2021-08-21 14:24:53 -0700

    Add image and more info to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import champagneImage from "./../img/champagne.jpg"
 
 function Header(){
   return (
-    <h1>Ferment Collective Tap Room</h1>
+    <React.Fragment>
+      <h1>Ferment Collective Tap Room</h1>
+      <img src={champagneImage} alt="An image of jelly jar with champagne next to a candle" />
+      <p>In house application to track inventory of our house-made fermented drinks on tap. We sell unique house-made wines, champagnes and kombuchas, sourced from home grown and locally foraged ingredients</p>
+    </React.Fragment>
   );
 }
 
